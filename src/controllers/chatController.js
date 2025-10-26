06630b6d861382f6f941599ae7d1d2363bb2d0b5
Maria Fernanda Guerrero Calderón <169078581+senshimarier@users.noreply.github.com>
2025-10-25 21:45:46 -0600

    Delete src directory

--- src/controllers/chatController.js
+++
@@ -1,7 +0,0 @@
-/*exports.chatWithAI = async (req, res) => {
-  const { question, type } = req.body;  // type opcional
-  const context = await loadFinancialData(type || 'personales');
-  const response = await getAIResponse(question, context);
-  res.json({ response });
-};
-*/
--- src/routes/chatRoutes.js
+++
@@ -1,6 +0,0 @@
-// chatRoutes.js
-const express = require('express');
-const { chatWithAI } = require('../controllers/chatController');
-const router = express.Router();
-router.post('/', chatWithAI);
-module.exports = router;
